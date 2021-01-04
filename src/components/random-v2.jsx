4840063f912957bbb1b44c9e6602088ff826e369
Oguzhan <47873038+oguzthmc@users.noreply.github.com>
2021-01-04 22:13:46 +0300

    finishing touches and end of the app

---
+++ src/components/random-v2.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import useGif from '../useGif';
+
+const Tag = () => {
+  const { gif, fetchGif } = useGif();
+
+  return (
+    <div className="container">
+      <h1>Random Gif</h1>
+      <img width="500" src={gif} alt="random-gif" />
+      <button onClick={fetchGif}>Click For New</button>
+    </div>
+  );
+}
+
+export default Tag;
--- src/components/tag-v1.jsx
+++ src/components/tag-v1.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const Tag = () => {
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+const Random = () => {
+  const [tag, setTag] = useState('dogs');
+  const [gif, setGif] = useState('');
+
+  const fetchGif = async (tag) => {
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tag}`;
+
+    const { data } = await axios.get(url);
+
+    const imageSrc = data.data.images.downsized_large.url;
+
+    setGif(imageSrc);
+  }
+
+  useEffect(() => {
+    fetchGif(tag);
+  }, [tag]);
+
+  const handleClick = () => {
+    fetchGif(tag);
+  }
+
   return (
-    <>
-      <h1>Tag</h1>
-    </>
+    <div className="container">
+      <h1>Random {tag} Gif</h1>
+      <img width="500" src={gif} alt="random-tag-gif" />
+      <input value={tag} onChange={(e) => setTag(e.target.value)} />
+      <button onClick={handleClick}>Click For New</button>
+    </div>
   );
 }
 
-export default Tag;
+export default Random;
